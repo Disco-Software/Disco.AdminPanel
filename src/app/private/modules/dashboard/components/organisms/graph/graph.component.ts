084beb0bcec1a1e2e95d7cfb3671fc755f6b354e
makhnivchyk <makhnivchyk@gmail.com>
2023-07-30 15:07:51 +0300

    fixed script error, added 2 more graphs, fixed layout component height

--- src/app/private/modules/dashboard/components/organisms/graph/graph.component.ts
+++ src/app/private/modules/dashboard/components/organisms/graph/graph.component.ts
@@ -25,12 +25,6 @@ export class GraphComponent implements OnInit {
                   borderColor: documentStyle.getPropertyValue('--blue-500'),
                   data: [65, 59, 80, 81, 56, 55, 40]
               },
-              {
-                  label: 'My Second dataset',
-                  backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-                  borderColor: documentStyle.getPropertyValue('--pink-500'),
-                  data: [28, 48, 40, 19, 86, 27, 90]
-              }
           ]
       };
 
