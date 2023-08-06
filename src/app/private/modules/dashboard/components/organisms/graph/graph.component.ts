72191ee5003d642aca5909f52ac7d9a9fb0f3da7
stas korchevskyi <stas_1999_nr@ukr.net>
2023-08-06 12:00:25 +0300

    (feat/DS-19-Graphs) Added a graphic

--- src/app/private/modules/dashboard/components/organisms/graph/graph.component.ts
+++ src/app/private/modules/dashboard/components/organisms/graph/graph.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.scss']
+  styleUrls: ['./graph.component.scss'],
 })
 export class GraphComponent implements OnInit {
   data: any;
@@ -11,58 +11,58 @@ export class GraphComponent implements OnInit {
   options: any;
 
   ngOnInit() {
-      const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--text-color');
-      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('#FFFFFF');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
-      this.data = {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [
-              {
-                  label: 'My First dataset',
-                  backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-                  borderColor: documentStyle.getPropertyValue('--blue-500'),
-                  data: [65, 59, 80, 81, 56, 55, 40]
-              },
-          ]
-      };
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+      ],
+    };
 
-      this.options = {
-          maintainAspectRatio: false,
-          aspectRatio: 0.8,
-          plugins: {
-              legend: {
-                  labels: {
-                      color: textColor
-                  }
-              }
+    this.options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          display: false,
+          labels: {
+            color: textColor,
           },
-          scales: {
-              x: {
-                  ticks: {
-                      color: textColorSecondary,
-                      font: {
-                          weight: 500
-                      }
-                  },
-                  grid: {
-                      color: surfaceBorder,
-                      drawBorder: false
-                  }
-              },
-              y: {
-                  ticks: {
-                      color: textColorSecondary
-                  },
-                  grid: {
-                      color: surfaceBorder,
-                      drawBorder: false
-                  }
-              }
-
-          }
-      };
+        },
+      },
+      scales: {
+        x: {
+          display: true,
+        },
+        y: {
+          display: false,
+          gridLines: {
+            //display: false,
+            lineWidth: 0
+          },
+          // gridLines: {
+          //   display : false,
+          //   ticks: {
+          //     display: false,
+          //   }
+          // }
+        },
+        // yAxes:
+        //   {
+        //     display: false,
+        //   },
+      },
+    };
   }
-
 }
