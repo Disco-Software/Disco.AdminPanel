e2616ee78001808b6ff5537880442df91b1cf2fa
stas korchevskyi <stas_1999_nr@ukr.net>
2023-10-18 19:02:57 +0300

    (ref/DS-108-Move-svg-code-to-own-svg-files) Fix of comments

--- src/app/private/components/organisms/notification/notification.component.ts
+++ src/app/private/components/organisms/notification/notification.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template: `<button class="bg-transparent border-0">
-  <img src="../../../../../assets/images/ic_notifications.svg">
+  <img [src]="'/assets/images/ic_notifications.svg'">
 </button>
 `
 })
