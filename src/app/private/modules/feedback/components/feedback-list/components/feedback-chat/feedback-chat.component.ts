998e66b31a34fa44475ae35bb6309d07f066ccc8
makhnivchyk <makhnivchyk@gmail.com>
2023-12-30 12:04:16 +0200

    Feedback Module: chat handle scroll

--- src/app/private/modules/feedback/components/feedback-list/components/feedback-chat/feedback-chat.component.ts
+++ src/app/private/modules/feedback/components/feedback-list/components/feedback-chat/feedback-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
@@ -6,12 +6,21 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
   templateUrl: './feedback-chat.component.html',
   styleUrls: ['./feedback-chat.component.scss']
 })
-export class FeedbackChatComponent implements OnInit {
-
+export class FeedbackChatComponent implements OnInit, AfterViewInit {
+  @ViewChild('chatBlock', { static: false }) chatBlock: ElementRef;
   status = 'closed'
-  constructor(private _activeModal : NgbActiveModal) { }
+  constructor(private _activeModal : NgbActiveModal, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    const chatBlockElement = this.chatBlock.nativeElement;
+    chatBlockElement.scrollTop = chatBlockElement.scrollHeight;
   }
 
   public onClose() : void{
