c087cffc6ae2beebea60b4d70bda804a62b99e5b
makhnivchyk <makhnivchyk@gmail.com>
2024-02-07 20:25:22 +0200

    Feedbacks Module: DS-174: fixed CORS

--- src/app/private/modules/feedback/components/feedback-list/components/feedback-chat/feedback-chat.component.ts
+++ src/app/private/modules/feedback/components/feedback-list/components/feedback-chat/feedback-chat.component.ts
@@ -39,26 +39,14 @@ export class FeedbackChatComponent implements AfterViewInit {
   }
 
   private startSignalRConnection(): void {
-    const headers: MessageHeaders = {
-      'Connection': 'Upgrade',
-      'Upgrade': 'websocket',
-      'Sec-WebSocket-Version': '13',
-      'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Credentials' : 'true',
-      'Access-Control-Allow-Methods' : 'ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL',
-      'Access-Control-Allow-Headers' : 'Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If',
-      'Access-Control-Expose-Headers' : 'DAV, content-length, Allow'
-    };
 
     const httpConnectionOptions : signalR.IHttpConnectionOptions = {
-      headers: headers,
+      withCredentials: false,
       accessTokenFactory : () => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsIm5iZiI6MTcwNzMyNDcwMCwiZXhwIjoxNzA3Mzk2NzAwLCJpc3MiOiJkaXNjby1hcGkiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0L0Rpc2NvLkFwaSJ9.Sxd47rzXm1WPq5RAI_SocqbR1bIsAuf2i-_qHGKCAx0',
     }
 
     this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl('http://localhost:5000/hub/ticket', httpConnectionOptions)
+      .withUrl('https://devdiscoapi.azurewebsites.net/hub/ticket', httpConnectionOptions)
       .build();
 
     this.hubConnection.start()
