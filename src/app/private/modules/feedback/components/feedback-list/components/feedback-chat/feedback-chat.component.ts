f4a40ff55b35a316e0cd6ac200ab59b96ce978b4
stas korchevskyi <stas_1999_nr@ukr.net>
2024-03-14 17:36:00 +0300

    (DS-174-chat-edit) implemented another context menu for componion messages

--- src/app/private/modules/feedback/components/feedback-list/components/feedback-chat/feedback-chat.component.ts
+++ src/app/private/modules/feedback/components/feedback-list/components/feedback-chat/feedback-chat.component.ts
@@ -7,41 +7,46 @@ import {
   OnDestroy,
   OnInit,
   Output,
-  ViewChild
+  ViewChild,
 } from '@angular/core';
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import * as signalR from '@microsoft/signalr';
-import {MessageHeaders} from '@microsoft/signalr';
-import {FeedbackInterface, FeedbackState, GetFeedbackMessagesAction, LocalStorageService} from "@core";
-import {Select, Store} from "@ngxs/store";
-import {map, Observable, take} from "rxjs";
-import {MessageRequestInterface} from 'src/app/core/models/ticket-chat/message-request.interface';
-import {environment} from "../../../../../../../../environments/environment";
-import {User} from "../../../../../../../core/models/account/change-email-response.model";
-import {InputComponent} from "@shared";
-import {MenuItem} from "primeng/api";
+import { MessageHeaders } from '@microsoft/signalr';
+import {
+  FeedbackInterface,
+  FeedbackState,
+  GetFeedbackMessagesAction,
+  LocalStorageService,
+} from '@core';
+import { Select, Store } from '@ngxs/store';
+import { map, Observable, take } from 'rxjs';
+import { MessageRequestInterface } from 'src/app/core/models/ticket-chat/message-request.interface';
+import { environment } from '../../../../../../../../environments/environment';
+import { User } from '../../../../../../../core/models/account/change-email-response.model';
+import { InputComponent } from '@shared';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-feedback-chat',
   templateUrl: './feedback-chat.component.html',
-  styleUrls: ['./feedback-chat.component.scss']
+  styleUrls: ['./feedback-chat.component.scss'],
 })
 export class FeedbackChatComponent implements OnInit, OnDestroy {
   @ViewChild('chatBlock') chatBlock: ElementRef;
   @ViewChild(InputComponent) inputComponent: InputComponent;
 
-  @Output() closeWindowEmitter = new EventEmitter()
+  @Output() closeWindowEmitter = new EventEmitter();
 
   @Input() ticket: FeedbackInterface;
-  @Input() message : string;
-  @Input() isOpen : boolean = true;
+  @Input() message: string;
+  @Input() isOpen: boolean = true;
 
   @Select(FeedbackState.isLoadingSelector) isLoading$: Observable<boolean>;
   isLoading: boolean = true;
   isSendingMessage = false;
   editableMessage: any;
-  isEdit : boolean = false;
-  Edited : boolean = false;
+  isEdit: boolean = false;
+  Edited: boolean = false;
 
   private hubConnection: signalR.HubConnection | undefined;
   public messages: any[] = [];
@@ -49,7 +54,7 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
   test: any[] = [];
   protected selectedContextMenuItem: any;
 
-  protected contextMenuItems: MenuItem[] = [
+  protected myContextMenuItems: MenuItem[] = [
     {
       label: 'Edit',
       icon: 'pi pi-pencil',
@@ -64,34 +69,74 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
       icon: 'pi pi-trash',
       iconClass: 'text-white me-3',
       command: (): void => {
-        this.hubConnection.invoke('delete-for-all', this.selectedContextMenuItem.id).then((): void => {
-          if (this.selectedContextMenuItem) {
-          this.messages = this.messages.map(message => {
-            if (message.id === this.selectedContextMenuItem.id) {
-              return {
-                ...message,
-                isRemoving: true,
-              };
-            } else {
-              return message;
+        this.hubConnection
+          .invoke('delete-for-all', this.selectedContextMenuItem.id)
+          .then((): void => {
+            if (this.selectedContextMenuItem) {
+              this.messages = this.messages.map((message) => {
+                if (message.id === this.selectedContextMenuItem.id) {
+                  return {
+                    ...message,
+                    isRemoving: true,
+                  };
+                } else {
+                  return message;
+                }
+              });
+              setTimeout((): void => {
+                this.messages = this.messages.map((message) => {
+                  if (message.id === this.selectedContextMenuItem.id) {
+                    return {
+                      ...message,
+                      isRemoving: false,
+                    };
+                  } else {
+                    return message;
+                  }
+                });
+                this.selectedContextMenuItem = null;
+              }, 1000);
             }
           });
-            setTimeout((): void => {
-            this.messages = this.messages.map(message => {
-              if (message.id === this.selectedContextMenuItem.id) {
-                return {
-                  ...message,
-                  isRemoving: false
-                };
-              } else {
-                return message
-              }
-            })
-              this.selectedContextMenuItem = null;
-          }, 1000);
-          }
+      },
+    },
+  ];
 
-        });
+  protected componionContextMenuItems: MenuItem[] = [
+    {
+      label: 'Delete',
+      icon: 'pi pi-trash',
+      iconClass: 'text-white me-3',
+      command: (): void => {
+        this.hubConnection
+          .invoke('delete-for-all', this.selectedContextMenuItem.id)
+          .then((): void => {
+            if (this.selectedContextMenuItem) {
+              this.messages = this.messages.map((message) => {
+                if (message.id === this.selectedContextMenuItem.id) {
+                  return {
+                    ...message,
+                    isRemoving: true,
+                  };
+                } else {
+                  return message;
+                }
+              });
+              setTimeout((): void => {
+                this.messages = this.messages.map((message) => {
+                  if (message.id === this.selectedContextMenuItem.id) {
+                    return {
+                      ...message,
+                      isRemoving: false,
+                    };
+                  } else {
+                    return message;
+                  }
+                });
+                this.selectedContextMenuItem = null;
+              }, 1000);
+            }
+          });
       },
     },
   ];
@@ -105,13 +150,18 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
   myUser: User;
   isRemoving: boolean = false;
 
-  constructor(private _activeModal: NgbActiveModal, private lsService: LocalStorageService, private store: Store, private cdr: ChangeDetectorRef) {
-  }
-
+  constructor(
+    private _activeModal: NgbActiveModal,
+    private lsService: LocalStorageService,
+    private store: Store,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.myUser = this.lsService.getItem('user').userName;
-    this.status = this.statuses.find(status => status.includes(this.ticket.status.toLowerCase()));
+    this.status = this.statuses.find((status) =>
+      status.includes(this.ticket.status.toLowerCase())
+    );
 
     this.startSignalRConnection();
   }
@@ -121,13 +171,13 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
     chatBlockElement.scrollTop = Math.max(
       chatBlockElement.scrollHeight,
       chatBlockElement.offsetHeight,
-      chatBlockElement.clientHeight,
-    )
+      chatBlockElement.clientHeight
+    );
   }
 
   public onClose(): void {
     this.isOpen = false;
-    setTimeout(x => {
+    setTimeout((x) => {
       this._activeModal.close();
       this.closeWindowEmitter.emit();
     }, 500);
@@ -137,20 +187,20 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
     const user = this.lsService.getItem('user');
     const accessToken = this.lsService.getString('accessToken');
 
-    const headers : MessageHeaders = {
+    const headers: MessageHeaders = {
       //"Content-Type" : "application/json",
-      "Connection" : "Upgrade",
-      "Upgrade" : "websocket",
-      "Sec-WebSocket-Version" : "13",
-      "Sec-WebSocket-Extensions" : "permessage-deflate; client_max_window_bits",
-      "User-Agent" : navigator.userAgent,
-    }
+      Connection: 'Upgrade',
+      Upgrade: 'websocket',
+      'Sec-WebSocket-Version': '13',
+      'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+      'User-Agent': navigator.userAgent,
+    };
 
-    const httpConnectionOptions : signalR.IHttpConnectionOptions = {
+    const httpConnectionOptions: signalR.IHttpConnectionOptions = {
       headers: headers,
       withCredentials: false,
-      accessTokenFactory : () => accessToken,
-    }
+      accessTokenFactory: () => accessToken,
+    };
 
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl(
@@ -159,17 +209,18 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
       )
       .build();
 
-    this.hubConnection.start()
+    this.hubConnection
+      .start()
       .then(() => {
         const req = {
           groupId: this.ticket.id,
           userId: user.id,
           pageNumber: 1,
           pageSize: 500,
-        }
+        };
         this.getAllMessages(req);
       })
-      .catch(err => null);
+      .catch((err) => null);
 
     this.subscribeMessages();
 
@@ -181,23 +232,25 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
   }
 
   getAllMessages(req: any): void {
-    this.store.dispatch(new GetFeedbackMessagesAction(req)).pipe(take(1), map(state => state.FeedbackState.messages)).subscribe(res => {
-      this.messages = res;
-      this.generateMapDates()
-      this.isLoading = false;
-      setTimeout(() => {
-        this.scrollToBottom();
+    this.store
+      .dispatch(new GetFeedbackMessagesAction(req))
+      .pipe(
+        take(1),
+        map((state) => state.FeedbackState.messages)
+      )
+      .subscribe((res) => {
+        this.messages = res;
+        this.generateMapDates();
+        this.isLoading = false;
+        setTimeout(() => {
+          this.scrollToBottom();
+        });
       });
-
-    })
   }
 
   subscribeMessages(): void {
     this.hubConnection.on('receive', (message: any) => {
-      this.messages = [
-        ...this.messages,
-        message
-      ];
+      this.messages = [...this.messages, message];
       this.generateMapDates();
       setTimeout((): void => {
         this.scrollToBottom();
@@ -206,23 +259,23 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
     });
   }
 
-  subscribeUpdate() : void {
+  subscribeUpdate(): void {
     this.hubConnection.on('update', (message: any) => {
-      const editableIndex = this.messages.findIndex(x => x.id === message.id);
-      if(editableIndex !== -1) {
+      const editableIndex = this.messages.findIndex((x) => x.id === message.id);
+      if (editableIndex !== -1) {
         this.messages = [
           ...this.messages.slice(0, editableIndex),
           message,
-          ...this.messages.slice(editableIndex + 1)
+          ...this.messages.slice(editableIndex + 1),
         ];
       }
-    })
+    });
   }
 
   subscribeStatuses(): void {
     this.hubConnection.on('changeStatus', (status: string): void => {
-      this.status = this.statuses.find(statusItem => {
-        return statusItem.includes(status.toLowerCase())
+      this.status = this.statuses.find((statusItem) => {
+        return statusItem.includes(status.toLowerCase());
       });
     });
   }
@@ -230,63 +283,75 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
   subscribeRemoveMessages(): void {
     this.hubConnection.on('remove', (id: number): void => {
       this.isRemoving = true;
-      setTimeout(x => {
-        this.messages = this.messages.filter(message=>message.id !== id);
+      setTimeout((x) => {
+        this.messages = this.messages.filter((message) => message.id !== id);
         this.generateMapDates();
         this.isRemoving = false;
       }, 1000);
-    })
+    });
   }
 
   generateMapDates(): void {
-    this.messageDates = this.messages.map(message =>
-      message.createdDate ?
-        message.createdDate.split('T')[0] : message.created.split('T')[0]
+    this.messageDates = this.messages.map((message) =>
+      message.createdDate
+        ? message.createdDate.split('T')[0]
+        : message.created.split('T')[0]
     );
-    this.messageDates = this.messageDates.filter((value, index) => this.messageDates.indexOf(value) === index)
+    this.messageDates = this.messageDates.filter(
+      (value, index) => this.messageDates.indexOf(value) === index
+    );
   }
 
   public filterMessagesByDate(date: string): any[] {
-    return this.messages.filter(message =>
-      message.createdDate ? message.createdDate?.includes(date) : message.created?.includes(date)
+    return this.messages.filter((message) =>
+      message.createdDate
+        ? message.createdDate?.includes(date)
+        : message.created?.includes(date)
     );
   }
 
-  public sendMessage(search : string): void {
+  public sendMessage(search: string): void {
     if (search) {
       this.isSendingMessage = true;
-      const chatMessage : MessageRequestInterface = {
+      const chatMessage: MessageRequestInterface = {
         message: search,
         ticketName: this.ticket.name,
-        ticketId: this.ticket.id
+        ticketId: this.ticket.id,
       };
 
-      this.hubConnection?.invoke('send', chatMessage.message, chatMessage.ticketName, chatMessage.ticketId)
+      this.hubConnection
+        ?.invoke(
+          'send',
+          chatMessage.message,
+          chatMessage.ticketName,
+          chatMessage.ticketId
+        )
         .then((res) => {
           this.isSendingMessage = false;
           this.inputComponent.clearMessageString();
 
           setTimeout((): void => {
             this.inputComponent.focusInput();
-          })
+          });
         })
-        .catch(err => null);
+        .catch((err) => null);
     }
   }
 
   protected editMessage(search: any): void {
     this.isSendingMessage = true;
-    this.hubConnection.invoke('update', search.id, search.message).then(res => {
-      this.editableMessage = null;
-      this.isSendingMessage = false;
-      this.inputComponent.clearMessageString();
+    this.hubConnection
+      .invoke('update', search.id, search.message)
+      .then((res) => {
+        this.editableMessage = null;
+        this.isSendingMessage = false;
+        this.inputComponent.clearMessageString();
 
-      setTimeout((): void => {
-        this.inputComponent.focusInput();
-      })
-    })
+        setTimeout((): void => {
+          this.inputComponent.focusInput();
+        });
+      });
   }
-
 
   changeStatus(event: any): void {
     const status: any = {
@@ -294,23 +359,23 @@ export class FeedbackChatComponent implements OnInit, OnDestroy {
       status: event.value.split('status.')[1],
     };
 
-    this.hubConnection?.invoke('updateStatus', status.id, status.status)
+    this.hubConnection
+      ?.invoke('updateStatus', status.id, status.status)
       .then((res) => {
-        console.log('successful status change')
+        console.log('successful status change');
       })
-      .catch(err => null);
-
+      .catch((err) => null);
   }
   getTime(date: string): string {
     let hours = new Date(date).getHours();
     let minutes = new Date(date).getMinutes();
 
-    let period = hours >= 12 ? "pm" : "am";
+    let period = hours >= 12 ? 'pm' : 'am';
 
     hours = hours % 12;
     hours = hours ? hours : 12; // Година 0 в 12-годинному форматі - 12 година
 
-    return hours + ":" + (minutes < 10 ? "0" : "") + minutes + " " + period;
+    return hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' ' + period;
   }
 
   ngOnDestroy(): void {
